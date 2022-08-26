f4d84ea597cdc356a7fd95303699ae283dd3dbcd
MomJust5Minutes <111364997+MomJust5Minutes@users.noreply.github.com>
2022-08-26 09:14:05 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+int main()
+{
+    int Vrand[1000];
+    int N=1000, MA, ME;
+    int menu;
+    float media;
+
+    printf("Gerando 1000 valores aleatorios:\n\n");
+    vetor_random(Vrand,N);
+    print_vet(Vrand,N);
+
+    do
+    {
+        printf("\n\nDigite uma das opcoes:\n\t[1]: Bubble Sort\n\t[2]: Quick Sort__");
+        scanf("%d", &menu);
+    }
+    while(menu!=1 && menu!=2);
+
+    switch(menu)
+    {
+    case 1:
+        printf("\n\n\nBubble Sort:\n");
+        Bubble_Sort(Vrand,N);
+
+    case 2:
+        printf("\n\n\nQuick Sort:\n");
+        quick_sort(Vrand, 0, N - 1);
+        print_vet(Vrand,N);
+    }
+
+    media = media_vet(Vrand,N);
+    printf("\n\nMedia: %.3f", media);
+
+    mediana_vet(Vrand, N);
+
+    MA = maior(Vrand,N);
+    printf("\nMaior: %d", MA);
+
+    ME = menor(Vrand,N);
+    printf("\tMenor: %d ", ME);
+
+    getch();
+    return 0;
+}
